a918e768b6d0d5c572fad4a10963a164611eaa91
ayoblvck <60554621+ayoblvck@users.noreply.github.com>
2020-05-02 19:42:21 +0100

    Update javascript4.js

--- javascript4.js
+++ javascript4.js
@@ -1,22 +1,49 @@
-(function() {
-        var allQuestions = [{
+function() {
+    var allQuestions = [{
             question: "COVID-19 is caused by what organism?",
-            options: ["bacteria", "virus", "paramecium", "rodents"],
-            answer: 2
-        }, {
+            options: [
+                { title: "bacteria", correct: false },
+                { title: "virus", correct: true },
+                { title: "fungi", correct: false },
+                { title: "rodents", correct: false }
+            ]
+        },
+        {
             question: "What state in china was the first case of Covid-19 discovered in?",
-            options: ["Hong-Kong", "shanghai", "Wuhan", "Beijing"],
-            answer: 3
-        }, {
-            question: "Which is not a known symptom of covid-19?",
-            options: ["Fever", "Running nose", "Difficulty in breathing", "Toothache"],
-            answer: 4
-        }, {
+            options: [
+                { title: "Hong-Kong", correct: false },
+                { title: "shanghai", correct: false },
+                { title: "Wuhan", correct: true },
+                { title: "Beijing", correct: false }
+            ],
+
+        },
+        {
+            question: "Which is not a known symptom of (covid-19?",
+            options: [
+                { title: "Fever", correct: false },
+                { title: "Running nose", correct: false },
+                { title: "Difficulty in breathing", correct: false },
+                { title: "Toothache", correct: true }
+            ],
+        },
+        {
             question: "What does the 19 in covid-19 stand for? ?",
-            options: ["It consists of 19 types of viruses ", "It kills in 19 days", "It was discovered in the year 2019", "It consists of 19 sequences"],
-            answer: 3
-        }, {
+            options: [
+                { title: "It consists of 19 types of viruses", correct: false },
+                { title: "It kills in 19 days", correct: false },
+                { title: "It was discovered in the year 2019", correct: true },
+                { title: "It consists of 19 sequences", correct: false }
+            ],
+        },
+        {
             question: "Which of these ways can covid-19 not be transmitted?",
-            options: ["Through mosquito bites", "Through coughing and sneezing", "Through handshakes", "Through kissing"],
-            answer: 1
-        }];
+            options: [
+                { title: "Through mosquito bites", correct: true },
+                { title: "Through coughing and sneezing", correct: false },
+                { title: "Through handshakes", correct: false },
+                { title: "Through kissing", correct: false }
+            ],
+        }
+    ]
+}
