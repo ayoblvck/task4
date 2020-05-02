b5c6ff59393d35b0c89c70e4eb033b1c67033c4a
ayoblvck <60554621+ayoblvck@users.noreply.github.com>
2020-05-02 17:22:37 +0100

    Add files via upload

---
+++ javascript4.js
@@ -0,0 +1,22 @@
+(function() {
+        var allQuestions = [{
+            question: "COVID-19 is caused by what organism?",
+            options: ["bacteria", "virus", "paramecium", "rodents"],
+            answer: 2
+        }, {
+            question: "What state in china was the first case of Covid-19 discovered in?",
+            options: ["Hong-Kong", "shanghai", "Wuhan", "Beijing"],
+            answer: 3
+        }, {
+            question: "Which is not a known symptom of covid-19?",
+            options: ["Fever", "Running nose", "Difficulty in breathing", "Toothache"],
+            answer: 4
+        }, {
+            question: "What does the 19 in covid-19 stand for? ?",
+            options: ["It consists of 19 types of viruses ", "It kills in 19 days", "It was discovered in the year 2019", "It consists of 19 sequences"],
+            answer: 3
+        }, {
+            question: "Which of these ways can covid-19 not be transmitted?",
+            options: ["Through mosquito bites", "Through coughing and sneezing", "Through handshakes", "Through kissing"],
+            answer: 1
+        }];
